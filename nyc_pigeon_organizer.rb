ce3c8ddafec8a4a8057a5d32612bc97d9b6d1f67
github username <github email address>
2015-09-06 11:32:06 -0400

    pigeons

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newData = {}
+  data.each do |key,value|
+    value.each do |k,v|
+      v.each do |x|
+        newData[x] = {}
+      end
+    end
+  end
+  data.each do |key,value|
+    value.each do |k,v|
+      v.each do |x|
+        if key == :color && !newData[x][key]
+          newData[x][key] = [k.to_s]
+        elsif key == :color && newData[x][key]
+          newData[x][key] << k.to_s
+        else
+          newData[x][key] = [k.to_s]
+        end
+      end
+    end
+  end
+  return newData
 end
